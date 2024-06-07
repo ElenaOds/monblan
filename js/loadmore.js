2efa13859abe9b7a7c6e2473835b425793f8897e
Elena Pedko <elpg.ods@gmail.com>
2024-06-07 09:48:26 +0300

    more cards are added, logic for load more button is added

---
+++ js/loadmore.js
@@ -0,0 +1,17 @@
+(() => {
+    const loadMore = document.querySelector('.load-button');
+    const hidden = [...document.querySelectorAll('.hidden')];
+
+    loadMore.addEventListener('click', function(e) {
+        e.preventDefault();
+  
+        hidden.splice(0, 4).forEach(
+        elem => elem.classList.remove('hidden')
+        )
+  
+    if (hidden.length === 0) {
+        loadMore.classList.add('disabled');
+    }
+    });
+
+  })();
